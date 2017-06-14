5ed0ca3eb32dde37069a2deac92f9b0872ed7345
Bane.Shi <fengxiaotx@163.com>
2017-06-14 18:22:26 +0800

    [Bane.Shi]

--- ms-grpc-test/src/test/java/com/moensun/grpc/service/UserService.java
+++ ms-grpc-test/src/test/java/com/moensun/grpc/service/UserService.java
@@ -1,6 +1,7 @@
 package com.moensun.grpc.service;
 
 import com.moensun.grpc.annotations.GrpcService;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
 /**
@@ -14,9 +15,12 @@ import io.grpc.stub.StreamObserver;
 public class UserService extends GreeterGrpc.GreeterImplBase{
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-        HelloReply reply = HelloReply.newBuilder().setMessage(("Hello "+request.getName())).build();
-        System.out.println(reply.getMessage().toString());
-        responseObserver.onNext(reply);
-        responseObserver.onCompleted();
+        try{
+            HelloReply reply = HelloReply.newBuilder().setMessage(("Hello "+request.getName())).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }catch (Exception ex){
+            responseObserver.onError(Status.INTERNAL.withCause(ex).withDescription("err").asRuntimeException());
+        }
     }
 }
